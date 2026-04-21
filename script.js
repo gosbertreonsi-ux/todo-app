fce8aee3e544675d2527fceefd44909fd8dbf56d
Reonsi <gosbertreonsi@gmail.com>
2026-04-21 20:42:05 +0300

    Implement task array and delete functionality

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+let tasks =[];
+
 const input = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const list = document.getElementById("taskList");
+
+function renderTasks(){
+    list.innerHTML="";
+
+    tasks.forEach((task,index)=>{
+        const li = document.createElement("li");
+        li.textContent =task;
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent="Delete";
+
+        deleteBtn.addEventListener("click",()=>{
+            deleteTask(index);
+        });
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+    });
+}
 
 button.addEventListener("click",()=>{
     const text = input.value;
 
     if(text==="") return;
 
-    const li = document.createElement("li");
-    li.textContent = text;
-    list.appendChild(li);
-    input.value="";
+    tasks.push(text);
+    input.value ="";
+
+    renderTasks();
 });
+
+function deleteTask(index){
+    tasks.splice(index,1);
+    renderTasks();
+}
